b2d31a429e393435412dbab8ca6d8124c6ab5b67
abelabebayehu <abelperspectives@gmail.com>
2025-02-10 20:57:17 +0300

    added dictionry of lists

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Python script that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress.
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """ That returns information about his/her TODO list progress """
+    ID = int(sys.argv[1])
+    urlUser = "https://jsonplaceholder.typicode.com/users/{}".format(ID)
+    urlTODO = "https://jsonplaceholder.typicode.com/todos?userId={}".format(ID)
+    user = requests.get(urlUser).json()
+    TODO = requests.get(urlTODO).json()
+
+    list_tasks = []
+    for task in TODO:
+        if task.get("completed") is True:
+            list_tasks.append(task.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
+          len(list_tasks), len(TODO)))
+
+    for task in list_tasks:
+        print("\t {}".format(task))
---
+++ 0x15-api/README.md
@@ -0,0 +1,11 @@
+# 0x15. API
+___
+
+### General learning objectives
+
+
+### Tasks
+0. Gather data from an API
+1. Export to CSV
+2. Export to JSON
+3. Dictionary of list of dictionaries
